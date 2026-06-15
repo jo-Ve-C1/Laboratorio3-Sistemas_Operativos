a4059fc8fba3ff581a4b8de8cca5baebe78f618d
joel <joelvelaz2016@gmail.com>
2026-06-14 23:47:07 -0300

    commit:2 Implementacion del ciclo de vida, Proceso fork y wait

--- main.c
+++ main.c
@@ -20,6 +20,8 @@ void credito(char *archivo_montos, int p[]);
 
 int main(){
 
+	pid_t pid_credito, pid_debito;
+
 	printf("[Padre]: Iniciando \n");
 sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS,-1, 0);
 saldo = mmap(NULL, sizeof(double), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS,-1, 0);
@@ -32,7 +34,37 @@ exit (EXIT_FAILURE);
 sem_init(sem, 1, 1);
 *saldo = 0.0;
 
-printf("[Padre]: Memoria compartida inicializada. Saldo base: %.2lf\n", *saldo);
+printf("[Padre]: Creando Procesos Hijos...");
+
+pid_credito = fork();
+
+if (pid_credito<0){
+perror("error al crear el hijo de credito");
+exit(EXIT_FAILURE);
+}
+
+if(pid == 0){
+printf("[hijo credito]: Proceso lanzado (PID: %d)\n", getpid());
+//LLAMAR FUNCION
+
+exit(EXIT_SUCCES);
+}
+
+pid_debito = fork ();
+if(pid_debito <0){
+perror ("Error hijo debito");
+exit(EXIT_FAILURE);
+}
+
+if (pid_debito == 0){
+printf("[hijo debito]: proceso lanzado (PID: %d)\n", getpid());
+//funcion llamar
+exit(EXIT_SUCCES);
+}
+wait(NULL);
+wait(NULL);
+
+printf("[Padre]: Hijos Finalizado. Saldo FInal: %.2lf\n",*saldo);
 
 sem_destroy(sem);
 munmap(sem, sizeof(sem_t));
